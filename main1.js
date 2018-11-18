ef0dc2f453ea6f024b6efd332b250f4a16f212f9
Chirag Tubakad <chirag.tubakad@Chirags-MacBook-Air.local>
2018-11-18 23:13:28 +0530

    New files added successfully

---
+++ main1.js
@@ -0,0 +1,6 @@
+document.getElementById("bookingform").style.display = "none";
+document.getElementById("cost").style.display = "none";
+function BookNow(car) {
+        var x = document.getElementById("bookingform");
+        x.style.display = "block";
+}
