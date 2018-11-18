49031d98e8a09670e0c03f3311c938498b0c61ae
Nandakrishna <nandakrishna75@gmail.com>
2018-11-19 01:12:39 +0530

    Navigation Bar CSS

--- main1.js
+++ main1.js
@@ -4,3 +4,19 @@ function BookNow(car) {
         var x = document.getElementById("bookingform");
         x.style.display = "block";
 }
+
+//sticky navbar
+window.onscroll = function () { myFunction() };
+
+var navbar = document.getElementById("topnav");
+
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+        if (window.pageYOffset >= sticky) {
+                navbar.classList.add("sticky")
+        } else {
+                navbar.classList.remove("sticky");
+        }
+}
+//end:sticky navbar
